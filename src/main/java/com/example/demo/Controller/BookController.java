db7e844db39f9cd4913cfe7e9a83aef8d4f94418
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-12-26 14:15:08 +0530

    Feat:Book Controller

---
+++ src/main/java/com/example/demo/Controller/BookController.java
@@ -0,0 +1,52 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Models.Book;
+import com.example.demo.Repository.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookController {
+
+    private final BookRepository repo;
+
+    public BookController(BookRepository repo) {
+        this.repo = repo;
+    }
+
+
+    @GetMapping("/")
+    public String listBooks(Model model) {
+        model.addAttribute("books", repo.findAll());
+        return "index";
+    }
+
+
+    @GetMapping("/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "add-book";
+    }
+
+
+    @PostMapping("/save")
+    public String saveBook(@ModelAttribute Book book) {
+        repo.save(book);
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable Long id, Model model) {
+        model.addAttribute("book", repo.findById(id).orElseThrow());
+        return "edit-book";
+    }
+
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        repo.deleteById(id);
+        return "redirect:/";
+    }
+}
